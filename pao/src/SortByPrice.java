d53beff6ddb2a740a06398cf14d13ed5b2a00c0e
State Giulia Antonia <107078740+GiuliaAntonia@users.noreply.github.com>
2023-04-07 15:05:11 +0300

    Delete pao directory

--- pao/src/SortByPrice.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SortByPrice implements Comparator<Produs> {
-    @Override
-    public int compare(Produs p1, Produs p2)
-    {
-        return p1.getPret() - p2.getPret();
-    }
-}
